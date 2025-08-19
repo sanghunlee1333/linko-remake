aeb69c066c69978352588c00121f23eb6134d597
이상훈 <sanghunlee1333@gmail.com>
2025-08-19 23:39:12 +0900

    첫 커

---
+++ linko/src/main/java/com/linko/LinkoApplication.java
@@ -0,0 +1,13 @@
+package com.linko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkoApplication.class, args);
+	}
+
+}
